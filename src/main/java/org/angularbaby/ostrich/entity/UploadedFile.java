57e6d5ba61d7176faf33ce8b65e7688b14bd359b
BattleOoze510821 <83394742@qq.com>
2018-01-04 22:38:49 +0800

    添加文件表

--- src/main/java/org/angularbaby/ostrich/entity/UploadedFile.java
+++ src/main/java/org/angularbaby/ostrich/entity/UploadedFile.java
@@ -1,4 +1,48 @@
 package org.angularbaby.ostrich.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "UploadedFile")
 public class UploadedFile {
+
+    public UploadedFile(Long key, Long project, String filename){
+        this.key=key;
+        this.project=project;
+        this.filename=filename;
+        this.version=1.0f;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public void setKey(Long key) { this.key = key; }
+
+    public Long getProject() {
+        return project;
+    }
+
+    public void setProject(Long project){ this.project=project; }
+
+    public String getFilename() { return filename; };
+
+    public void setFilename(String filename) { this.filename=filename; }
+
+    public Float getVersion() { return version; };
+
+    public void setVersion(Float version) { this.version=version; }
+
+    @Id
+    @GeneratedValue
+    private Long key;
+
+    @Column(nullable = false)
+    private Long project;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private Float version;
 }
--- src/main/java/org/angularbaby/ostrich/repository/UploadedFileRepository.java
+++ src/main/java/org/angularbaby/ostrich/repository/UploadedFileRepository.java
@@ -3,6 +3,6 @@ package org.angularbaby.ostrich.repository;
 import org.angularbaby.ostrich.entity.UploadedFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<UploadedFile, Long> {
+public interface UploadedFileRepository extends JpaRepository<UploadedFile, Long> {
 
 }
