6c08f4aa746552f6f0fac70ef6b5839f6b2498b2
Qiu Chaofan <fwage73@gmail.com>
2018-01-05 04:04:11 +0800

    修复了不能正常登录的问题

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LocationStrategy } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Ng2DragDropModule } from 'ng2-drag-drop';
@@ -31,23 +31,11 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ChatService } from "./chat/chat.service";
 import { MatMenuModule, MatProgressSpinnerModule, MatTabsModule } from '@angular/material'
 import { DashboardComponent, DashboardNewDialogComponent, DashboardNewDialogGroupComponent } from './dashboard/dashboard.component'
-import { ChatComponent} from './chat/chat.component'
-import { CalendarService} from "./calendar/calendar.service"
-import { CalendarPrototypeComponent } from "./calendar/calendar/calendar"
-import { CalendarComponent } from "./calendar/calendar.component"
-import { FileComponent } from "./file/file.component"
-import { FileService } from "./file/file.service"
-import { FileUploadModule } from 'ng2-file-upload'
-import { LoginService } from './login/login.service'
 import { DashboardService } from './dashboard/dashboard.service'
-import { HttpModule } from '@angular/http'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MatMenuModule } from '@angular/material'
 import { ProjectListComponent, NewProjectDialogComponent, InviteMembersDialogComponent, ProjectDetailDialogComponent, LeaveProjectDialogComponent } from './project-list/project-list.component'
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component'
 import { ProjectListService } from './project-list/project-list.service'
 import { AuthModule } from './auth.module'
-import {DashboardService} from "./dashboard/dashboard.service";
 import { MatTooltipModule } from '@angular/material/tooltip'
 import { JoinInvitationComponent } from './project-list/join-invitation/join-invitation.component'
 import { RegisterService } from './register/register.service'
